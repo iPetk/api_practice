45cd9039f75c1a3501a77065d426d8d7a80f17ff
Irena Petkovic <petkovicirena1@gmail.com>
2022-10-26 19:22:22 +0200

    coordinates, bounds, user geolocation

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Travel advisor app
+Made using
+-React
+-MaterialUI
+-Axios
+
+APIs used:
+-Google maps
+-[Travel advisor](https://rapidapi.com/apidojo/api/travel-advisor/)
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -6,22 +6,26 @@ import Rating from '@material-ui/lab';
 
 import useStyles from './styles';
 
-export default function Map() {
+export default function Map({ setCoordinates, setBounds, coordinates }) {
   const classes = useStyles();
   const isMobile = useMediaQuery('(min-width:600px)');
 
-  const coordinates = { lat: 0, lng: 0 };
-
   return (
     <div className={classes.mapContainer}>
+      {console.log(coordinates)}
+
       <GoogleMapReact bootstrapURLKeys={{ key: 'AIzaSyD4bwSTUBOaEkoGoU3DdqhhnjIpfdHPSso'}}
-      defaultCenter={coordinates}
-      center={coordinates}
-      defaultZoom={14}
-      margin={[50, 50, 50, 50]}
-      options={''}
-      onChange={''}
-      onChildClick={''}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={''}
+        onChange={(e) => {
+          console.log(e);
+          setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+          setBounds({ ne: e.marginBounds.ne, sw: e.marginBoundssw });
+        }}
+        onChildClick={''}
       >
 
       </GoogleMapReact>
