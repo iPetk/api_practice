ba353e75060b76982537fb9c1d2ff63bfe7265bb
Irena Petkovic <petkovicirena1@gmail.com>
2022-10-26 15:30:32 +0200

    List + PlaceDetails

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export default function PlaceDetails() {
+export default function PlaceDetails({ place }) {
   return (
-    <h1>PlaceDetails</h1>
+    <h1>{place.name}</h1>
   );
 }
